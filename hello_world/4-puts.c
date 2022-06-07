eca0b3281cd78611db107e92474f0f8567e27d30
Maykha27 <5144@holbertonschool.com>
2022-06-07 03:57:59 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - Entry point 
  * Return: Always 0 (success)
  */
-
 int main()
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
