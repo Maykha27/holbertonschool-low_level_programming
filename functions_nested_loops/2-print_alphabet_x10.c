c9e795b94e30e25a0d716fe90ac1b3d7fd3de84f
Maykha27 <5144@holbertonschool.com>
2022-06-09 07:47:59 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-*main - Entry point
-*print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+* print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
 */
 void print_alphabet_x10(void)
 {
