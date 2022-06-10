118cc2eed61361422b313f04b90064ce8b8430a5
Maykha27 <5144@holbertonschool.com>
2022-06-10 10:22:13 -0700

    9-times_table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,6 +22,11 @@ if (y == 0)
 {
 _putchar('0');
 }
+else if (product >= 10)
+{
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
 else if ((product < 10) && (y != 0))
 {
 _putchar(' ');
