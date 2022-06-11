aa40ddfe2f0fdffa5ce80040975250f9500db81d
Maykha27 <5144@holbertonschool.com>
2022-06-11 12:30:57 -0700

    bcvfd

--- debugging/3-main_a.c
+++ debugging/3-main_a.c
@@ -14,14 +14,12 @@ int day;
 int year;
 
 month = 4;
-day = 01; 
+day = 01;
 year = 1997;
 
 printf("Date: %02d/%02d/%04d\n", month, day, year);
-
 day = convert_day(month, day);
 
 print_remaining_days(month, day, year);
-
 return (0);
 }
