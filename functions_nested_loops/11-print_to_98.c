2bedb1d4dad6340ac46377140b1505cb9e360ebf
Maykha27 <5144@holbertonschool.com>
2022-06-11 00:34:50 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - prints all natural numbers from n to 98, followed by a new line.
+ * Return: Always 0.
+*/
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	print_to_98(-10);
+	return (0);
+}
