d218fe90a9bcdf2400218aca9da1c7e54817b1af
Maykha27 <5144@holbertonschool.com>
2022-06-13 14:01:10 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_line - check of result
+  *@n: n - variable
+  * Return: Always (0)
+ */
+void print_line(int n)
+{
+int x;
+if (n > 0)
+{
+for (x = 1; x <= n; x++)
+{
+_putchar(95);
+}
+}
+_putchar('\n');
+}
