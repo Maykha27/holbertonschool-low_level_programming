1108b90374cead762edf4be350b43be274ef5aa4
Maykha27 <5144@holbertonschool.com>
2022-06-13 05:24:51 -0700

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Muttiplay tow integer
+ * @a: first integer
+ * @b: second integer
+ * Return: mul of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,5 +2,7 @@
 #define HEADER
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
 
 #endif
