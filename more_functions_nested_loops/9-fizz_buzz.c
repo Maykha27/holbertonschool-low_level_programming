6a738879d0a81c442b5b8b4fefb63e4f8425831f
Maykha27 <5144@holbertonschool.com>
2022-06-14 04:39:07 -0700

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,15 +11,18 @@ int i;
 
 	for (i = 1; i <= 100; ++i)
 	{
-		if (i % 3 == 0)
-			printf("FIZZ ");
-		else if (i % 5 == 0)
-			printf("BUZZ ");
-		else if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FIZZBUZZ ");
+		if ((i % 15) == 0)
+			printf("FIZZBUZZ");
+		else if ((i % 3) == 0)
+			printf("FIZZ");
+		else if ((i % 5) == 0)
+			printf("BUZZ");
 		else
 			printf("%d ", i);
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
-		printf("\n");
-		return (0);
+	printf("\n");
+	return (0);
 }
