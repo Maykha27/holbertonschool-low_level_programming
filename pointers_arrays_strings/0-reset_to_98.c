c3c06c042608a0751a5162d7b2a5065078f9c905
Maykha27 <5144@holbertonschool.com>
2022-06-15 04:39:23 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * reset_to_98 - check the result
+  * @n: parameter and updates the value
+  * Return: n
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
