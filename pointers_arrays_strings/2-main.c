c3bb715e99e20de7a6d00406873412afcbf033e0
Maykha27 <5144@holbertonschool.com>
2022-06-18 10:55:50 -0700

    2-strchr

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,15 +2,20 @@
 #include <stdio.h>
 
 /**
-* main - check the code
- * Return: Always 0.
-*/
+   * main - check the code
+    *
+     * Return: Always 0.
+      */
 int main(void)
 {
-	char *str;
-	int len;
-	str = "My first strlen!";
-	len = _strlen(str);
-	printf("%d\n", len);
-	return (0);
+	    char *s = "hello";
+	        char *f;
+
+		    f = _strchr(s, 'l');
+
+		        if (f != NULL)
+				    {
+					            printf("%s\n", f);
+						        }
+			    return (0);
 }
