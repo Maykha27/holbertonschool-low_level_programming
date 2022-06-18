f3c63d4fcc0b541f57de9c95b3fc81ae515a7c01
Maykha27 <5144@holbertonschool.com>
2022-06-18 09:46:08 -0700

    1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _memcpy - copy memory
+  * @dest: dest momery
+  * @src: source memory area
+  * @n: bytes
+  * Return: memory eare
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a <  n; a++)
+	{
+dest[a] = src[a];
+	}
+return (dest);
+}
