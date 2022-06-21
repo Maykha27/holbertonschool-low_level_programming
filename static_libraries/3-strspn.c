9b120d512b4b69d78fcdf21cbdc39716be011f6f
Maykha27 <5144@holbertonschool.com>
2022-06-21 05:40:39 -0700

    nvc

--- static_libraries/3-strspn.c
+++ static_libraries/3-strspn.c
@@ -1,15 +1,33 @@
 #include "main.h"
-/**
-   * _strcmp - function that compares two strings.
-    * @s1: input
-     * @s2: input
-      * Return: output
-       */
-int _strcmp(char *s1, char *s2)
-{
-		int i = 0;
 
-			while (s1[i] == s2[i] && s1[i] != '\0')
-						i++;
-				return (s1[i] - s2[i]);
+/**
+   * _strspn - Entry point
+    * @s: input
+     * @accept: input
+      * Return: Always 0 (Success)
+       */
+unsigned int _strspn(char *s, char *accept)
+{
+		unsigned int i, n, value, check;
+
+			value = 0;
+
+				for (i = 0; s[i] != '\0'; i++)
+						{
+									check = 0;
+
+											for (n = 0; accept[n] != '\0'; n++)
+														{
+																		if (accept[n] == s[i])
+																						{
+																											value++;
+																															check = 1;
+																																		}
+																				}
+
+													if (check == 0)
+																	return (value);
+														}
+
+					return (value);
 }
