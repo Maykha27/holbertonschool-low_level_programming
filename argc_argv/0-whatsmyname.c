4ba269251319eac2bf059d2b5938d29cef1198eb
Maykha27 <5144@holbertonschool.com>
2022-06-22 04:24:34 -0700

    mynameis

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - prints the program
+  *@argv: array of pointers to strings
+  *@argc: argument count
+  *Return: void
+  */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
