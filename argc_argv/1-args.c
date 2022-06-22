7484d58c263968e35bfc025fc918b1fc067fc4f4
Maykha27 <5144@holbertonschool.com>
2022-06-22 05:21:24 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *main - argument
+  *@argc: the number argument
+  *@argv: vector
+  *Return: void
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+		printf("%d\n", argc - 1);
+	return (0);
+}
+
