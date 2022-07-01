afb0a908828094a421adfe55662d152a5901d0d6
Maykha27 <5144@holbertonschool.com>
2022-07-01 11:55:29 -0700

    3-fo

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *op_add - function addition
+  *@a: first num
+  *@b: two num
+  *
+  *Return: void
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  *op_sub - function subvention
+  *@a:...
+  *@b:...
+  *Return:..
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  *op_div - function division
+  *@a:...
+  *@b:....
+  *Return:...
+  */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+  *op_mod - function modulo
+  *@a:...
+  *@b:....
+  *Return:...
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("\n");
+		exit(100);
+	}
+	return (a % b);
+}
