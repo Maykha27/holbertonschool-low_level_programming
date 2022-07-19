aefe8d93431be3a11ea9b5fc9d9fdb87bd4a27b7
Maykha27 <5144@holbertonschool.com>
2022-07-19 15:52:51 +0100

    3-set_bit.c

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets the value of a bit to 1 at a given index.
+ * @n: the numbers
+ * @index: he index, starting from 0 of the bit you want to set
+ *
+ * Return:
+ *
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+
