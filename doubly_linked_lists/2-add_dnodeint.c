3800971a6014aa4e92ea896135d9fcf1f853e6f0
Maykha27 <5144@holbertonschool.com>
2022-07-23 22:20:23 +0100

    2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - function that adds a new node at the beginning
+ * add_dnodeint - function that adds a new node at the beginning
  * @head: A pointer to the head of the dlisrint_t list
  * @n:the integer for the new node to contain
  *
@@ -8,17 +8,17 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *last;
+	dlistint_t *new;
 
-	new = malloc (sizeof (dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	return (NULL);
 
 	new->n = n;
-	new-> prev = NULL;
-	new-> next = *head;
+	new->prev = NULL;
+	new->next = *head;
 	if (*head != NULL)
-	(*head)-> prev = new;
+	(*head)->prev = new;
 	*head = new;
 
 	return (new);
